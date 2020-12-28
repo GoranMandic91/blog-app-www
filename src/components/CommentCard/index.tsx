4cf8ce4aec62e5323f82148852180a0c90d2f1e6
Goran Mandic <godzi1991@gmail.com>
2020-12-28 22:48:13 +0100

    Improve design

--- src/components/CommentCard/index.tsx
+++ src/components/CommentCard/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import './index.css';
 import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
 import IconButton from '@material-ui/core/IconButton';
 import { CommentsActionTypes } from '../../store/comments/types';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from "@material-ui/core/Avatar";
+
 interface PostCardProps {
   id: number;
   name: string;
@@ -16,17 +17,20 @@ interface PostCardProps {
 export default function CommentCard({ name, text, openDialog }: PostCardProps) {
   return (
     <Card className="CommentCard" raised>
-      <CardContent className="Content">
-        <Typography variant="body1" color="textPrimary" component="h1">
-          {name}
-          <IconButton className="FloatRight" color="primary" size="small" onClick={openDialog}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe">
+            {name[0] || ':)'}
+          </Avatar>
+        }
+        action={
+          <IconButton color="primary" size="small" onClick={openDialog}>
             <EditTwoToneIcon />
           </IconButton>
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="h1">
-          {text}
-        </Typography>
-      </CardContent>
+        }
+        title={name}
+        subheader={text}
+      />
     </Card>
   );
 }
--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
 import Badge from '@material-ui/core/Badge';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import AddCommentOutlinedIcon from '@material-ui/icons/AddCommentOutlined';
-import './index.css';
-import { Link } from "react-router-dom";
 import IconButton from '@material-ui/core/IconButton';
 import { CommentsActionTypes } from '../../store/comments/types';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from "@material-ui/core/Avatar";
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import './index.css';
 
 interface PostCardProps {
   id: number;
@@ -23,32 +23,45 @@ interface PostCardProps {
 
 export default function PostCard({ id, title, content, numOfComments, hidden, openDialog }: PostCardProps) {
   return (
-    <Card className="PostCard" raised>
-      <CardContent >
-        <Typography gutterBottom variant="h5" component="h2">
-          {title}
-          {!hidden &&
-            <Badge className="FloatRight" badgeContent={numOfComments} color="primary" showZero>
-              <ChatOutlinedIcon color="primary" />
-            </Badge>
-          }
-          {hidden &&
-            <IconButton className="FloatRight" color="primary" size="small" onClick={openDialog}>
-              <AddCommentOutlinedIcon />
-            </IconButton>
-          }
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {content}
-        </Typography>
-      </CardContent>
-      {!hidden &&
-        <CardActions className="FloatRight">
-          <Button component={Link} to={`/posts/${id}`} variant="contained" color="primary">
-            Open post
-          </Button>
-        </CardActions>
-      }
-    </Card>
+    <>
+      {hidden && (
+        <Card className="PostCard" raised>
+          <CardHeader
+
+            action={
+              <IconButton color="primary" size="small" onClick={openDialog}>
+                <AddCommentOutlinedIcon />
+              </IconButton>
+            }
+            title={title}
+            subheader={content}
+          />
+        </Card>
+      )}
+      {!hidden && (
+        <Card className="PostCard" raised>
+          <CardHeader
+            className="PostCardHeader"
+            avatar={
+              < Avatar aria-label="recipe">
+                {title[0] || content[0]}
+              </Avatar>
+            }
+            action={
+              <IconButton >
+                <Badge badgeContent={numOfComments} color="primary" showZero>
+                  <ChatOutlinedIcon color="primary" />
+                </Badge>
+              </IconButton>
+            }
+            title={title}
+            subheader={content.substring(0, 80) + '...'}
+          />
+          <CardActions className="FloatRight">
+            <Button component={Link} to={`/posts/${id}`} size="small" color="primary">Read More</Button>
+          </CardActions>
+        </Card>
+      )}
+    </>
   );
 }
