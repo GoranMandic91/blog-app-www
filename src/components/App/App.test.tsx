f02bfde79340a7e24a4e14470ab0166ccc47dabb
Goran Mandic <godzi1991@gmail.com>
2020-12-28 23:35:37 +0100

    Fix tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import App from './App';
 
+const mockStore = configureStore();
+
+jest.mock('../CommentDialog', () => ({
+  __esModule: true,
+  default: () => <p>CommentDialog</p>,
+}));
+
 test('renders learn react link', () => {
-  render(<App fetchPostsRequest={() => { }} />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = render(
+    <Provider store={mockStore()}>
+      <App />
+    </Provider>
+  );
+
+  expect(app.getByText(/Simple Blog Application/i)).toBeInTheDocument();
 });
